4b756d6a285984519c36ba5154d0a76dc4635c8f
gioanbatan <giovannibassani@hotmail.com>
2022-11-11 16:17:16 +0100

    Add: Dynamic select

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ export default {
   },
   methods: {
     changeSeries() {
-      if
+      console.log("");
     }
   }
 }
--- src/components/AppSelectCategory.vue
+++ src/components/AppSelectCategory.vue
@@ -15,10 +15,11 @@ export default {
 <template>
     <div class="container mx-auto">
         <div class="ms_category-container">
-            <select @change="$emit('selectSeries')" id="category">
-                <option selected="selected" value="series[0]">{{ series[0] }}</option>
-                <option value="series[1]">{{ series[1] }}</option>
-                <option value="series[2]">{{ series[2] }}</option>
+            <select @change="$emit('selectSeries')" id="category" v-model="store.seriesSelected">
+                <option selected value="" disabled>Select category</option>
+                <option v-for="(selectedCategory, index) in series" :value="selectedCategory" :key="index">{{
+                        selectedCategory
+                }}</option>
             </select>
         </div>
     </div>
